e006f851e6a530be0ab127cd1148d205072e1fb3
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-05-12 20:08:22 -0700

    Media queries added

--- index.php
+++ index.php
@@ -56,6 +56,7 @@ else{
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device width, initial-scale=1.0">
 	<title>AppAcademy API</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
