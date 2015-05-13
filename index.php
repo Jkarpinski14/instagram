9dca8974c0e71fb1019ba661cc8f8e4bbc3b7ffb
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-05-12 19:18:48 -0700

    Milestone 6 completed & main.css created

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ session_start(); //Commences the session
 //Make constants using define
 define('clientID', 'bb199a42cd21404fadf74e1564d369b5');
 define('clientSecret', '21dec25dae844aa5b8edad461cd4803d');
-define('redirectURI', 'http://localhost:8888/appacademyapi/index.php');
+define('redirectURI', 'http://localhost/appacademyapi/index.php');
 define('ImageDirectory', 'pics/'); //Saves pictures to the server
 
-if isset(($_GET['code'])){
+//isset dertermines if a variable is set and not NULL
+if (isset($_GET['code'])){
 	$code = (($_GET['code']));
 	$url = 'https://api.instagram.com/oauth/access_token';
 	$access_token_settings = array('client_id' => clientID, 
@@ -26,12 +27,15 @@ if isset(($_GET['code'])){
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>AppAcademy API</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 	<!--Creating a login for people to go and give approval for our web app to access their Instagram account-->
 	<!--After getting approval, we are going to have the information so that we can play with it-->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">LOGIN</a> <!--Response will always be code-->
+	<div class="linkdiv">
+		<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a> <!--Response will always be code-->
+	</div>
 	<script type="text/javascript" src=""></script>
 </body>
 </html>
