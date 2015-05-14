77c592b4dc2fb40fee96df160517897dd8e032f7
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-05-14 09:06:01 -0700

    Milestone 12 completed

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ function getUserID($userName){
 	$instagramInfo = connectToInstagram($url);
 	$results = json_decode($instagramInfo, true);
 
-	echo $results['data']['0']['id'];
+	return $results['data']['0']['id'];
 }
 
 //Function to print images onto screen
